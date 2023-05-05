a6cff4ac085856fc1ef79d802e9e669848f912d7
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-05-05 10:18:39 -0500

    create programa_09

---
+++ src/test/java/org/uv/practica09/Practica09ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.uv.practica09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
