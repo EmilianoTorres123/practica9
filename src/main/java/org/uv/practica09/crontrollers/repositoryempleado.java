ab22682d3507676bca4153a75d0b2540bf9088c2
emiliano <106941242+EmilianoTorres123@users.noreply.github.com>
2023-05-08 09:45:44 -0500

    updatee programa_09

---
+++ src/main/java/org/uv/practica09/crontrollers/repositoryempleado.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package org.uv.practica09.crontrollers;
+
+import org.springframework.data.repository.CrudRepository;
+
+
+/**
+ *
+ * @author emili
+ */
+public interface repositoryempleado extends CrudRepository<Empleado, Long>  {
+    
+}
